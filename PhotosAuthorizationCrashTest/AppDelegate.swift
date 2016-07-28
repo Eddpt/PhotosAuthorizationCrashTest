14f51b483f10c51ce5f55f3c2d8d7aa9c8debeb8
Eddpt <Eddpt@users.noreply.github.com>
2016-07-28 18:18:56 +0100

    Add initial project structure

---
+++ PhotosAuthorizationCrashTest/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PhotosAuthorizationCrashTest
+//
+//  Created by Eddpt on 28/07/2016.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ PhotosAuthorizationCrashTest/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  PhotosAuthorizationCrashTest
+//
+//  Created by Eddpt on 28/07/2016.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
